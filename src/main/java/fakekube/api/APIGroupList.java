26064951047c8b3819b7df3a7c44e6421dd8bbdd
Arik Hadas <ahadas@redhat.com>
2020-01-14 15:28:12 +0200

    read /apis from file

--- src/main/java/fakekube/api/APIGroupList.java
+++ src/main/java/fakekube/api/APIGroupList.java
@@ -7,15 +7,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import io.kubernetes.client.openapi.JSON;
 import io.kubernetes.client.openapi.models.V1APIGroupList;
 
 @WebServlet(name = "APIGroupList", urlPatterns = {"/apis"}, loadOnStartup = 1)
 public class APIGroupList extends KubeServlet<V1APIGroupList> {
+	public static final String API_GROUP_LIST_FILE = "/fakekube/api/groups.json";
 
 	protected V1APIGroupList query(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		V1APIGroupList apiGroupList = new V1APIGroupList();
-		apiGroupList.setKind("APIGroupList");
-		apiGroupList.setApiVersion("v1");
-		return apiGroupList;
+		String str = readFromFile(API_GROUP_LIST_FILE);
+		return new JSON().getGson().fromJson(str, V1APIGroupList.class);
 	}
 }
