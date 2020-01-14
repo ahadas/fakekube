c7801001ff6ee775be7a8b3c4e88210ba4bf8b64
Arik Hadas <ahadas@redhat.com>
2020-01-14 15:16:41 +0200

    read /api/v1 from file

--- src/main/java/fakekube/api/APIResourceList.java
+++ src/main/java/fakekube/api/APIResourceList.java
@@ -1,8 +1,6 @@
 package fakekube.api;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -16,22 +14,10 @@ import io.kubernetes.client.openapi.models.V1APIResourceList;
 @WebServlet(name = "APIResourceList", urlPatterns = {"/api/v1"}, loadOnStartup = 1)
 public class APIResourceList extends KubeServlet<V1APIResourceList> {
 	private static final Logger LOGGER = Logger.getLogger(APIResourceList.class.getName());
-	public static final String API_RESOURCE_LIST_FILE = "/fakekube/api/V1APIResourceList.json";
+	public static final String API_RESOURCE_LIST_FILE = "/fakekube/api/resources.json";
 
 	protected V1APIResourceList query(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		byte[] bytes;
-		try {
-			InputStream inputStream = getClass().getClassLoader().getResourceAsStream(API_RESOURCE_LIST_FILE);
-			bytes = new byte[inputStream.available()];
-			inputStream.read(bytes);
-		} catch (IOException e) {
-			LOGGER.severe("failed to load APIResourceList, return empty list");
-			V1APIResourceList l = new V1APIResourceList();
-			l.setKind("APIResourceList");
-			l.setApiVersion("v1");
-			l.setResources(new ArrayList<>());
-			return l;
-		}
-		return new JSON().getGson().fromJson(new String(bytes), V1APIResourceList.class);
+		String str = readFromFile(API_RESOURCE_LIST_FILE);
+		return new JSON().getGson().fromJson(str, V1APIResourceList.class);
 	}
 }
