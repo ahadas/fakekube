820717588921340fe58ffc2b6d0d492c2c3ce398
Arik Hadas <ahadas@redhat.com>
2020-02-13 15:53:13 +0200

    send metrics through remote-write

--- src/main/java/fakekube/io/sim/RemoteWrite.java
+++ src/main/java/fakekube/io/sim/RemoteWrite.java
@@ -5,9 +5,11 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -54,31 +56,34 @@ public class RemoteWrite {
 	}
 
 	private HttpRequest.BodyPublisher buildData() throws IOException {
+		long timestamp = new Date().getTime();
 		Enumeration<MetricFamilySamples> enumeration = registry.filteredMetricFamilySamples(Collections.emptySet());
 		WriteRequest.Builder writeRequestBuilder = Remote.WriteRequest.newBuilder();
 		while (enumeration.hasMoreElements()) {
 			MetricFamilySamples samples = enumeration.nextElement();
-//			List<Label> labels = samples.samples.stream().
+			List<TimeSeries> protoTimeSeries = new ArrayList<>();
+
+			for (MetricFamilySamples.Sample sample : samples.samples) {
+				TimeSeries.Builder timeSeriesBuilder = TimeSeries.newBuilder();
+				List<Sample> protoSamples = new ArrayList<>();
+				List<Label> protoLabels = new ArrayList<>();
+
+				protoLabels.add(Label.newBuilder().setName("__name__").setValue(sample.name).build());
+				int numOfLabels = sample.labelNames.size();
+				for (int i=0; i<numOfLabels; i++) {
+					String name = sample.labelNames.get(i);
+					String value = sample.labelValues.get(i);
+					protoLabels.add(Label.newBuilder().setName(name).setValue(value).build());
+				}
+
+				Sample protoSample = Sample.newBuilder().setValue(sample.value).setTimestamp(timestamp).build();
+				TimeSeries timeSeries = timeSeriesBuilder.addAllLabels(protoLabels).addSamples(protoSample).build();
+				protoTimeSeries.add(timeSeries);
+			}
+			writeRequestBuilder.addAllTimeseries(protoTimeSeries);
 		}
 			
-			WriteRequest request = Remote.WriteRequest.newBuilder()
-					.addTimeseries(
-							TimeSeries.newBuilder()
-							// https://github.com/influxdata/influxdb/pull/9599
-							.addLabels(Label.newBuilder().setName("__name__").setValue("arik"))
-							.addLabels(Label.newBuilder().setName("worker").setValue("derez"))
-							.addSamples(Sample.newBuilder().setValue(4).setTimestamp(new Date().getTime()).build())
-							//						.gets
-							.build())
-					.addTimeseries(TimeSeries.newBuilder()
-							.addLabels(Label.newBuilder().setName("__name__").setValue("derez"))
-							.addLabels(Label.newBuilder().setName("arik").setValue("b"))
-							.addSamples(Sample.newBuilder().setValue(6).setTimestamp(new Date().getTime()).build())
-							//						.gets
-							.build())
-					.build();
-			
-//		WriteRequest request = writeRequestBuilder.build();
+		WriteRequest request = writeRequestBuilder.build();
 		byte[] compressed = Snappy.compress(request.toByteArray());
 		return HttpRequest.BodyPublishers.ofByteArray(compressed);
 	}
