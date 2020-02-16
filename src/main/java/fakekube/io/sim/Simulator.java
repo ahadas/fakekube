5789b211f5ce179fc26e9fd48348074261bb3d2a
Arik Hadas <ahadas@redhat.com>
2020-02-16 11:51:41 +0200

    separate out the addition of default namespace from the model

--- src/main/java/fakekube/io/sim/Simulator.java
+++ src/main/java/fakekube/io/sim/Simulator.java
@@ -7,7 +7,11 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+import fakekube.io.model.IoK8sApiCoreV1Namespace;
+import fakekube.io.model.IoK8sApiCoreV1NamespaceStatus;
+import fakekube.io.model.IoK8sApimachineryPkgApisMetaV1ObjectMeta;
 import fakekube.io.sim.metrics.Metrics;
+import fakekube.io.sim.model.Namespaces;
 
 @WebServlet(name = "Simulator", loadOnStartup = 1, urlPatterns = {"/simulator"})
 public class Simulator extends HttpServlet {
@@ -17,11 +21,23 @@ public class Simulator extends HttpServlet {
 	private Clock clock;
 	@Inject
 	private Metrics metrics;
+	@Inject
+	private Namespaces namespaces;
 
 	@Override
 	public void init() throws ServletException {
 		LOGGER.info("starting simulator servlet");
 		clock.init();
+		addDefaultNamespace();
 		metrics.init();
+	}
+
+	private void addDefaultNamespace() {
+		IoK8sApiCoreV1Namespace defaultNamespace = new IoK8sApiCoreV1Namespace()
+				.apiVersion("v1")
+				.kind("Namespace")
+				.metadata(new IoK8sApimachineryPkgApisMetaV1ObjectMeta().name("default").creationTimestamp(clock.creationTimestamp()))
+				.status(new IoK8sApiCoreV1NamespaceStatus().phase("Active"));
+		namespaces.add(defaultNamespace);
 	}
 }
